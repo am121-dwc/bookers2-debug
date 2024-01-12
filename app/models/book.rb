59f87a59f7b1fae027657fe99b7e3eb9a73ea701
am121-dwc <aoimorisugi121@gmail.com>
2024-01-12 09:20:01 +0000

    model controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   get 'favorites/create'
   root to: 'homes#top'
   get 'home/about'
