2f02033ff62573f6fe3fa8287a81764c460db37e
am121-dwc <aoimorisugi121@gmail.com>
2024-01-25 06:53:57 +0000

    0125

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,9 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
 
+  def new
+    @group = Group.new
+  end
   def index
     @book = Book.new
     @groups = Group.all
