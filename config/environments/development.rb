07d67ee796b078f1cb017664610db84df1e5989e
am121-dwc <aoimorisugi121@gmail.com>
2024-01-27 07:04:17 +0000

    test2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     address:             'smtp.gmail.com',
     port:                 587,
     domain:              'gmail.com',
-    user_name:           'xxxxxxxxxgmail.com',
+    user_name:           'yyyyy@gmail.com',
     password:            'kvjd vtpf ehei xfaz',
     authentication:      'login',
     enable_starttls_auto: true
