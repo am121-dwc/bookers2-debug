ee13bfadb9525a1fc161e8dffe01ee8269061fb1
am121-dwc <aoimorisugi121@gmail.com>
2024-01-27 07:28:58 +0000

    ignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     address:             'smtp.gmail.com',
     port:                 587,
     domain:              'gmail.com',
-    user_name:           'xxx@gmail.com',
+    user_name:           'Y@gmail.com',
     password:            'kvjd vtpf ehei xfaz',
     authentication:      'login',
     enable_starttls_auto: true
