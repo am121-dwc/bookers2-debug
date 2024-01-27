4a8bffe70625862df24369d683c7f6e15d083f60
am121-dwc <aoimorisugi121@gmail.com>
2024-01-27 10:20:17 +0000

    send_mail view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorite, only: [:create, :destroy]
