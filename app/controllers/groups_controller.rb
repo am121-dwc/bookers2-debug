6eef940aeb61df83330ad63f6bd90dc614c29b91
am121-dwc <aoimorisugi121@gmail.com>
2024-01-28 03:24:48 +0000

    mailer

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -39,14 +39,14 @@ class GroupsController < ApplicationController
   def new_mail
     @group = Group.find(params[:group_id])
   end
-  def send_email
+  def send_mail
     @group = Group.find(params[:group_id])
     group_users = @group.users
     @mail_title = params[:mail_title]
     @mail_content = params[:mail_content]
     GroupMailer.send_mail(@mail_title, @mail_content,group_users).deliver
-    
-    
+
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,17 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_for :users
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
-    resource :group_users, only: [:create, :destroy]
-    get "new/mail" => "groups#new_mail"
-    get "send/mail" => "groups#send_mail"
+  resource :group_users, only: [:create, :destroy]
+  get "new/mail" => "groups#new_mail"
+  get "send/mail" => "groups#send_mail"
   end
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
-    resource :favorite, only: [:create, :destroy]
-    resources :book_comments, only: [:create, :destroy]
+  resource :favorite, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update] do
-    member do
-      get :follows, :followers
+  member do
+  get :follows, :followers
     end
       resource :relationships, only: [:create, :destroy]
   end
