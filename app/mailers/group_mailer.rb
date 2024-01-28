e0d6849c1d2f282665cfab924ac36fe17db9ad9e
am121-dwc <aoimorisugi121@gmail.com>
2024-01-28 05:17:38 +0000

    mailer ok

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,5 +1,6 @@
 class GroupMailer < ApplicationMailer
-  def send_mail(mail_title, mail_content, group_users)
+  def send_mail(mail_title, mail_content, group_users, group_id)
+    @group = Group.find(group_id)
     @mail_title = mail_title
     @mail_content = mail_content
     mail bcc: group_users.pluck(:email), subject: mail_title
